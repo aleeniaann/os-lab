ca54029c9401ee77736d0855efe051ab9be9e99c
unknown <aleeniaann@gmail.com>
2022-10-06 20:55:32 +0300

    update

--- sjf_nonpre.c
+++ sjf_nonpre.c
@@ -1,1 +1,42 @@
 //sjf non-preemptive
+#include<stdio.h>
+int sjf_tat=0,sjf_wt=0,i,j,t,bt[30],p[30],tot_tat=0,tot_wt=0,n;
+
+void main(){
+
+    printf("\nenter no of processes: ");
+    scanf("%d",&n);
+
+    printf("\nenter burst time of each process: ");
+    for(i=0;i<n;i++){
+    scanf("%d",&bt[i]);
+
+    }
+
+    sort();
+    sjf(&sjf_tat,&sjf_wt);
+    //print the stuff..
+
+}
+
+int sort(){
+    for(i=0;i<n;i++){
+        for(j=i+1;j<n;j++){
+            if(bt[i]>bt[j]){
+                swap(&bt[j],&bt[i]);
+                swap(&p[j],&p[i]);
+            }
+        }
+    }
+
+    swap(int *a, int *b){
+        t=*a;
+        *a=*b;
+        *b=t;
+    }
+    return 0;
+}
+
+int sjf(int *a, int *b){
+    
+}
