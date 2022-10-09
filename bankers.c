812864fa9a716ad542b94a16c637e87feaf2e89e
unknown <aleeniaann@gmail.com>
2022-10-09 10:09:22 +0300

    bankers

--- bankers.c
+++ bankers.c
@@ -1,1 +1,43 @@
 //bankers algorithm
+
+#include<stdio.h>
+int max[100][100],alloc[100][100],avail[100];
+int i,j,n,r;
+
+void main(){
+    input();
+    show();
+    cal();
+
+}
+
+void input(){
+    printf("\nEnter no of processes: ");
+    scanf("%d",&n);
+    printf("\nEnter no of resource instances: ");
+    scanf("%d",&r);
+
+    printf("\nEnter the max matrix: ");
+    for(i=0;i<n;i++){
+        for(j=0;j<r;j++){
+            scanf("%d",&max[i][j]);
+        }
+    }
+
+    printf("\nEnter the allocation matrix: ");
+    for(i=0;i<n;i++){
+        for(j=0;j<r;j++){
+            scanf("%d",&alloc[i][j]);
+        }
+    }
+    
+    printf("\nEnter the available resources: ");
+        for(j=0;j<r;j++){
+            scanf("%d",&avail[j]);
+        }
+
+}
+
+void show(){
+    
+}
